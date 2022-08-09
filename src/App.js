88582175fcc1505124651c93f35863c698b28a58
nate <nate@Nates-MacBook-Pro.local>
2022-08-09 18:00:37 -0500

    title change

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h6>
           JerseyTalk
-        </p>
+        </h6>
       </header>
     </div>
   );
